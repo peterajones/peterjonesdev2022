6bbe401c48927907de5ad1da90b9c3c937aaa9db
Peter Jones <peter@peter-jones.ca>
2022-01-13 15:52:20 -0500

    add pagination and password generator projects

--- pages/projects/pagination/index.js
+++ pages/projects/pagination/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Data from '../../../Components/projects/pagination/Data';
