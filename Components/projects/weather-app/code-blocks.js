54728b5581eaa2e5db17b23de314032bcdb3ae5b
Peter Jones <peter@peter-jones.ca>
2022-01-14 11:51:10 -0500

    remove 'import React' statement

--- Components/projects/weather-app/code-blocks.js
+++ Components/projects/weather-app/code-blocks.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Link from 'next/link';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
