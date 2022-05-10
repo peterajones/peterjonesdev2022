d89c7e6eda3797ee345dd410861274abfa570add
Peter Jones <peter@peter-jones.ca>
2022-05-09 21:23:01 -0400

    add netlify sso

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ import EmailProvider from "next-auth/providers/email"
 import GithubProvider from "next-auth/providers/github"
 import GitlabProvider from "next-auth/providers/gitlab"
 import GoogleProvider from "next-auth/providers/google"
-// import NetlifyProvider from "next-auth/providers/netlify";
+import NetlifyProvider from "next-auth/providers/netlify";
 
 export default NextAuth({
   // DB Adapter
@@ -32,10 +32,10 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
-    // NetlifyProvider({
-    //   clientId: process.env.NETLIFY_CLIENT_ID,
-    //   clientSecret: process.env.NETLIFY_CLIENT_SECRET
-    // }),
+    NetlifyProvider({
+      clientId: process.env.NETLIFY_CLIENT_ID,
+      clientSecret: process.env.NETLIFY_CLIENT_SECRET
+    }),
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET
