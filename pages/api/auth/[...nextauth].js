4bf3e901330797f9f0f483d81d1cf2873f8604f8
Peter Jones <peter@peter-jones.ca>
2022-06-05 17:08:29 -0400

    add email login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ import EmailProvider from "next-auth/providers/email"
 import GithubProvider from "next-auth/providers/github"
 import GitlabProvider from "next-auth/providers/gitlab"
 import GoogleProvider from "next-auth/providers/google"
-// import NetlifyProvider from "next-auth/providers/netlify";
+import NetlifyProvider from "next-auth/providers/netlify";
 const sgMail = require("@sendgrid/mail")
 
 export default NextAuth({
@@ -17,17 +17,17 @@ export default NextAuth({
   },
   // Configure one or more authentication providers
   providers: [
-    EmailProvider({
-      server: {
-        host: process.env.SENDGRID_SERVER,
-        port: process.env.SENDGRID_PORT,
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_SMTP_PASSWORD,
-        }
-      },
-      from: process.env.EMAIL_FROM
-    }),
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.SENDGRID_SERVER,
+    //     port: process.env.SENDGRID_PORT,
+    //     auth: {
+    //       user: process.env.SENDGRID_USERNAME,
+    //       pass: process.env.SENDGRID_SMTP_PASSWORD,
+    //     }
+    //   },
+    //   from: process.env.EMAIL_FROM
+    // }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
@@ -40,10 +40,10 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
-    // NetlifyProvider({
-    //   clientId: process.env.NETLIFY_CLIENT_ID,
-    //   clientSecret: process.env.NETLIFY_CLIENT_SECRET
-    // }),
+    NetlifyProvider({
+      clientId: process.env.NETLIFY_CLIENT_ID,
+      clientSecret: process.env.NETLIFY_CLIENT_SECRET
+    }),
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET
